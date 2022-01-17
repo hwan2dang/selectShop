d8ea46ce041b2b0a40cd55667e6d82f49066dcfb
Seunghwan Jung <67155560+hwan2dang@users.noreply.github.com>
2022-01-17 15:22:42 -0500

    Add code

---
+++ SelectShopApplication.java
@@ -0,0 +1,17 @@
+package com.project.selectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling // 스프링 부트에서 스케줄러가 작동하도록 합니다.
+@EnableJpaAuditing //시간 자동 변경이 가능하도록 합니다.
+@SpringBootApplication // 스트링 부트임을 선언합니다.
+public class SelectShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectShopApplication.class, args);
+    }
+
+}
